d4d288505a5a5f3d6c253d0594e2f7c28b76b114
theodorCodes <laphan.fan@gmail.com>
2021-06-13 01:57:52 +0800

    Test app.js file setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,6 @@ c Marks: Advanced levels
 d Marks: Styling and animation
 t Marks:
 */
+
+// t: Test if index.html loads app.js file
+console.log("t1: Link from app.js to index.html established: Ok");
