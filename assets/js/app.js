b0c827e7ae54266ddd277f03c9c6ed8fde4e88c5
theodorCodes <laphan.fan@gmail.com>
2021-06-13 01:56:18 +0800

    app.js file setup

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/*
+Memory Game - Comments Structure
+a Marks: Bare minimum code to get it work and playable
+b Marks: Fixes to make the game more stable and prevent obvious bugs
+c Marks: Advanced levels
+d Marks: Styling and animation
+t Marks:
+*/
