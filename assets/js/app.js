dce9e659fa808e36b910136b7bc012373a512c64
theodorCodes <laphan.fan@gmail.com>
2021-06-13 02:01:11 +0800

    Created playing cards in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,4 +17,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
   // t: Test if event listener works to check if DOM is loaded
   console.log("t2: The Dom has loaded: Ok");
 
+  // a2)
+  // Create array with objects of cards
+  const playingCards = [
+
+    // with image name and path for each img
+    // Each card is repeated to create pairs
+    {name: "card1", img: "assets/images/card1.png"},
+    {name: "card1", img: "assets/images/card1.png"},
+    {name: "card2", img: "assets/images/card2.png"},
+    {name: "card2", img: "assets/images/card2.png"},
+    {name: "card3", img: "assets/images/card3.png"},
+    {name: "card3", img: "assets/images/card3.png"},
+    {name: "card4", img: "assets/images/card4.png"},
+    {name: "card4", img: "assets/images/card4.png"},
+    {name: "card5", img: "assets/images/card5.png"},
+    {name: "card5", img: "assets/images/card5.png"},
+    {name: "card6", img: "assets/images/card6.png"},
+    {name: "card6", img: "assets/images/card6.png"}
+
+  ];
+  
 });
